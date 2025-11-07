cb8031687a5aec82b37f72188a6263a7226da1fe
Anuj Sharma <anujsharma11746@gmail.com>
2025-11-08 00:47:57 +0530

    Api Fetching

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const API_BASE = import.meta.env.MODE === "development"
   ? "http://localhost:5000/api"
-  : "https://to-do-app-.vercel.app/api";
+  : import.meta.env.VITE_API_BASE || "https://to-do-app-.vercel.app/api";
 
 const axiosInstance = axios.create({
   baseURL: API_BASE,
