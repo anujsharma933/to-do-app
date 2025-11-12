e59145e665feed285f04ab0a707fe96f811a802b
Anuj Sharma <anujsharma11746@gmail.com>
2025-11-12 12:03:57 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,9 @@ const app = express();
 
 // Middlewares
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin:"*"
+}));
 
 const PORT = process.env.PORT || 5000;
 const MONGO_URI = process.env.MONGO_URI;
